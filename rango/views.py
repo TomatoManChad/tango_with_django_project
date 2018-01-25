eb38ddab059fad6714cb47304fc17d726885a955
TomatoManChad <spankthemonkey15@hotmail.co.uk>
2018-01-25 18:44:21 +0000

    changes to first changes of ch6.2

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,22 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {'categories': category_list}
+    
     # return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+    #this is reponse being rendered back
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
