1cb44296cf83c4dcba7d682ec152c2920d0b548c
TomatoManChad <spankthemonkey15@hotmail.co.uk>
2018-01-25 21:28:10 +0000

    ch6.2 modifying index template

--- rango/views.py
+++ rango/views.py
@@ -12,14 +12,14 @@ def index(request):
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
 
+    # this queries Category model to retrieve top five cate
     category_list = Category.objects.order_by('-likes')[:5]
-
     context_dict = {'categories': category_list}
-    
+
     # return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
     #this is reponse being rendered back
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_about = {'boldmessage': "The king of cats"}
