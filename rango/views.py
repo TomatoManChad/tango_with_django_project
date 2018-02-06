96baee4b515899b296bed84483f3546d633663d0
TomatoManChad <spankthemonkey15@hotmail.co.uk>
2018-02-06 22:50:45 +0000

    ch10 pre exercise

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ from  django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 # Create your views here.
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -20,17 +21,30 @@ def index(request):
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
 
+
+    request.session.set_test_cookie()
     # this queries Category model to retrieve top five cate
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    # Obtain our respnse object early so we can add cookie information
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # call the helper function to handle the cookies
+    # Return response back to the user, updating any cookies that need changed
+    return response
+
     # return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
-    #this is reponse being rendered back
-    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_about = {'boldmessage': "The king of cats"}
     # return HttpResponse("Rango says here is the about page!<br/> <a href='/rango/'>Index</a>")
     return render(request, 'rango/about.html', context=context_about)
@@ -221,3 +235,36 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage
     return HttpResponseRedirect(reverse('index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # we use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesnt exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If its been more than a day since the last visit...
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
